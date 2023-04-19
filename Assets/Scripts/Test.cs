76117f68b56f08732225c969f7bd6861b3316aff
Arman Karapetyan <>
2023-04-19 23:31:22 +0400

    ui testing

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+using bbInject;
+using Ui.Module;
+using Ui.View.Home;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private ViewService _viewService;
+
+    [Inject]
+    private void Inject(ViewService viewService)
+    {
+        _viewService = viewService;
+    }
+        
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _viewService.Show<HomeView, HomeViewController>(null);
+        }
+    }
+}
---
+++ Assets/Scripts/TestInstaller.cs
@@ -0,0 +1,11 @@
+using bbInject;
+using UnityEngine;
+
+public class TestInstaller:DependencyInstaller
+{
+    [SerializeField] private Test test;
+    public override void Install(Container container)
+    {
+        container.Bind<Test>().FromInstance(test).AsSingle();
+    }
+}
